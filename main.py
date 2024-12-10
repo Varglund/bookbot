f233704fdeb9dc017813f8754713fc914cd86ad3
Sean Varglund <sean.varglund@gmail.com>
2024-12-10 19:54:03 +0200

    print everything to console

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from pathlib import Path
+
+def get_text(path_to_file: Path):
+    with open(path_to_file) as fp:
+        text = fp.read()
+    return text
+
+def main()->None:
+    path_to_file = Path("books","frankenstein.txt")
+    print(get_text(path_to_file))
+    
+if __name__=="__main__":
+    main()
